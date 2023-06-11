c4a397215f17d7cf89289493c5a1933dab29eb73
markb <markbuchholz8@gmail.com>
2023-06-10 22:45:30 -0600

    added final edits

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, request
-import json
+#these are the imports
+from flask import Flask, request, make_response
+import json, jsonify
 import dbhelpers
 
 app = Flask(__name__)
-
+#the first get request will send a request that will run the sql procedure called return_philosopher
 @app.get("/api/philosopher")
 def return_philo():
     results = dbhelpers.run_procedures("CALL return_philosopher()", [])
@@ -12,4 +13,36 @@ def return_philo():
         return philosopher_json
     else:
         return "sorry, something went wrong"
+#the first post request runs by calling the sql procedure called new_philosopher needing 5 jsons this also has the make_response
+app.post("/api/philosopher")
+def create_philosopher():
+    error = dbhelpers.check_endpoint_info(request.json, ["name", "bio", "date_of_birth", "date_of_death", "image_url"])
+    if(error != None):
+        return make_response(jsonify(error), 400)
+    results = dbhelpers.run_procedures("call new_philosopher(?,?,?,?,?)", [request.json.get('name'), request.json.get('bio'), request.json.get('date_of_birth'), request.json.get('date_of_death'), request.json.get('image_url')])
+    if(type(results) == list):
+        return make_response(jsonify(results), 200)
+    else:
+        return make_response(jsonify("sorry, something has gone wrong"))
+#the second get request calls the sql procedure called return_all this will return all of the qoutes   
+@app.get("/api/qoute")
+def return_qoutes():
+    results = dbhelpers.run_procedures("CALL return_all()", [])
+    if(type(results) == list):
+        philosopher_json = json.dump(results, default=str)
+        return philosopher_json
+    else:
+        return "sorry, something went wrong"
+#the seconf post calls the sql procedure new_philosopher this will create a new philosopher it needs 2 jsons and it has the make_response
+app.post("/api/qoute")
+def create_qoutes():
+    error = dbhelpers.check_endpoint_info(request.json, ["philosopher_id", "content"])
+    if(error != None):
+        return make_response(jsonify(error), 400)
+    results = dbhelpers.run_procedures("call new_philosopher(?,?)", [request.json.get('philosopher_id'), request.json.get('content')])
+    if(type(results) == list):
+        return make_response(jsonify(results), 200)
+    else:
+        return make_response(jsonify("sorry, something has gone wrong"))
+#i set up the debugger
 app.run(debug=True)
